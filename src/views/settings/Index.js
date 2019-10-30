a99c18c53120a67833c43c2d2dcf8b250b759e20
@Inori <2665611998@qq.com>
2019-10-30 21:53:19 +0800

    update

---
+++ src/views/settings/Index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  View, Text,
+  StyleSheet
+} from 'react-native'
+
+export default class Settings extends React.Component{
+  static propTypes = {
+    
+  }
+
+  constructor (props){
+    super(props)
+    this.state = {
+      
+    }
+  }
+  
+
+  render (){
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  
+})
