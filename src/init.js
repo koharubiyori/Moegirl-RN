796df38659dcf1c2a1e60d6f7a481326d5bba042
@Inori <2665611998@qq.com>
2019-11-02 13:51:14 +0800

    fixbug

--- src/init.js
+++ src/init.js
@@ -5,15 +5,27 @@ import { logout } from './redux/user/HOC'
 import { check } from './redux/user/HOC'
 
 store.dispatch(dispatch =>{
-  storage.get('userName').then(name => dispatch({ type: SET_INFO, name }))
+  storage.get('userName').then(name =>{
+    if(!name){ return }
+    dispatch({ type: SET_INFO, name })
+
+    // 获取一次编辑令牌，判断登录状态是否有效
+    check().catch(() =>{
+      logout()
+      $dialog.confirm.show({
+        content: '登录状态貌似失效了，要前往登录吗？',
+        onTapCheck: () =>{
+          $appNavigator.current._navigation.push('login')
+        }
+      })
+    })    
+  })
 })
 
-check().catch(() =>{
-  logout()
-  $dialog.confirm.show({
-    content: '登录状态貌似失效了，要前往登录吗？',
-    onTapCheck: () =>{
-      $appNavigator.current._navigation.push('login')
-    }
+storage.get('config').then(config =>{
+  if(!config) storage.set('config', {
+    heimu: true,
+    biliPlayerReload: false,
+    immersionMode: false
   })
 })
