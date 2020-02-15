68ac4dace915bd5145a0f8a58bfe6eea517b356f
@Inori <2665611998@qq.com>
2020-02-15 14:24:27 +0800

    更新颜色

--- src/utils/moeRequest.ts
+++ src/utils/moeRequest.ts
@@ -6,32 +6,9 @@ export default function<ResponseData = any>(config: AxiosRequestConfig): Promise
   if (!config.params) config.params = {}
   config.params.format = 'json'
 
-  // const timeout = new Promise(resolve => setTimeout(() => resolve('timeout'), 12000))
-
-  // return new Promise((resolve, reject) => {
-  //   Promise.race([request(config), timeout])
-  //     .then(data => {
-  //       if (data === 'timeout') {
-  //         console.log('timeout', config)
-  //         return reject()
-  //       }
-      
-  //       data.error ? reject(data.error) : resolve(data)
-  //     })
-  //     .catch(e => {
-  //       console.log(e)
-  //       reject()
-  //     })
-  // })
-
   return new Promise((resolve, reject) => {
     request(config)
       .then(data => {
-        // if (data === 'timeout') {
-        //   console.log('timeout', config)
-        //   return reject()
-        // }
-        
         const { data: reqData } = data
         reqData.error ? reject(reqData.error) : resolve(reqData)
       })
